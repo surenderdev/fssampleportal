f4460ac7a59531be7533cc639f8f23037159b6f5
surenderdev <synind@gmail.com>
2023-04-11 18:31:01 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter  } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
+  <BrowserRouter basemname='/fssampleportal'>
     <App />
   </BrowserRouter>
 
